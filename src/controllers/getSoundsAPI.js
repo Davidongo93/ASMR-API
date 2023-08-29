e41ffb302d4424e23419cb0607192458bae136b6
Ssamza <santiagoemezeta@gmail.com>
2023-08-29 18:38:42 -0500

    update, delete id property in Sound model  because mongoDB create automatically this property

--- src/controllers/getSoundsAPI.js
+++ src/controllers/getSoundsAPI.js
@@ -1,47 +1,70 @@
 require('dotenv').config();
 const { Client } = require('podcast-api');
+const { Sound } = require('../db');
+const { get } = require('mongoose');
 const { API_KEY_DAVE, API_KEY_SAMZA } = process.env;
-const totalEpisodesToFetch = 10;
+const totalEpisodesToFetch = 80;
 
 const getSoundsAPI = async () => {
 	const client = Client({ apiKey: API_KEY_DAVE });
-	/* API Key Dave*/
-	// const client = Client({ apiKey: API_KEY_SAMZA }); /* API Key Samza*/
 	let allEpisodes = [];
 	let nextEpisodePubDate = null; // Initialize to null for the first fetch
 
 	while (allEpisodes.length < totalEpisodesToFetch) {
-		const response = await client.fetchPodcastById({
-			id: '407f99bd98764c48a8ce81fd9ce9a72f',
-			next_episode_pub_date: nextEpisodePubDate,
-			sort: 'recent_first',
-		});
+		try {
+			const response = await client.fetchPodcastById({
+				id: '407f99bd98764c48a8ce81fd9ce9a72f',
+				next_episode_pub_date: nextEpisodePubDate,
+				sort: 'recent_first',
+			});
 
-		const episodes = response.data.episodes;
-		// return episodes;
+			const episodes = response.data.episodes;
 
-		if (episodes.length === 0) {
-			break; // No more episodes available
+			if (episodes.length === 0) {
+				break; // No more episodes available
+			}
+
+			const episodesToFetch = Math.min(
+				totalEpisodesToFetch - allEpisodes.length,
+				episodes.length
+			);
+			const fetchedEpisodes = episodes
+				.slice(0, episodesToFetch)
+				.map((item) => ({
+					published_at: item.pub_date_ms,
+					title: item.title,
+					audio: item.audio,
+				}));
+
+			allEpisodes = [...allEpisodes, ...fetchedEpisodes];
+
+			nextEpisodePubDate = fetchedEpisodes[episodesToFetch - 1].published_at;
+
+			// Agregar un intervalo de 1 segundo entre las solicitudes
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+		} catch (error) {
+			console.error('Error:', error);
+			// Manejo de errores aquí
 		}
-
-		const episodesToFetch = Math.min(
-			totalEpisodesToFetch - allEpisodes.length,
-			episodes.length
-		);
-		const fetchedEpisodes = episodes.slice(0, episodesToFetch).map((item) => ({
-			id: item.id,
-			published_at: item.pub_date_ms,
-			title: item.title,
-			audio: item.audio,
-		}));
-
-		allEpisodes = [...allEpisodes, ...fetchedEpisodes];
-
-		// Update nextEpisodePubDate with the pub_date_ms of the last episode fetched
-		nextEpisodePubDate = fetchedEpisodes[episodesToFetch - 1].published_at;
 	}
 
 	return allEpisodes;
 };
 
-module.exports = getSoundsAPI;
+const soundsDB = async () => {
+	const count = await Sound.countDocuments();
+
+	if (count > 0) {
+		// Hay documentos en la base de datos, entonces obtén los sonidos de la DB
+		const existingSounds = await Sound.find();
+		return existingSounds;
+	} else {
+		const soundsToInsert = await getSoundsAPI(); // datos API
+
+		const sounds = await Sound.insertMany(soundsToInsert); // Insertar datos en la DB
+		return sounds;
+	}
+};
+
+// module.exports = getSoundsAPI;
+module.exports = soundsDB;
