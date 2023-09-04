c843f95f9439935f5d9cda16d3811b43281bccbf
Ssamza <santiagoemezeta@gmail.com>
2023-09-04 12:13:37 -0500

    update(database deployed with mongoDB Atlas - ASK for the URI to use it)

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const { DB_HOST, DB_PORT, DB_DATABASE, DB_DEPLOY } = process.env;
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_DEPLOY, DB_ATLAS } = process.env;
 
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+// mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// });
+mongoose.connect(DB_ATLAS, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
-// mongoose.connect(DB_DEPLOY, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error cant connect MongoDB:'));
