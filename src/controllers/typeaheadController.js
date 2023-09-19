9b4f78b34fab64908fc41c9091e381023357fabb
David Orlando Miranda <domirandar@unal.edu.co>
2023-09-19 17:33:17 -0500

    search suggestions(typeahead route)

---
+++ src/controllers/typeaheadController.js
@@ -0,0 +1,21 @@
+// typeaheadController.js
+const { Client } = require('podcast-api');
+const { LISTEN_API_KEY } = process.env;
+
+const typeaheadController = async (searchTerm) => {
+  try {
+    const client = Client({
+      apiKey: LISTEN_API_KEY || null,
+    });
+
+    const response = await client.typeahead({ q: searchTerm, show_podcasts: 1 });
+
+    const suggestions = response.data;
+
+    return suggestions;
+  } catch (error) {
+    throw new Error('Error rendering suggestions?');
+  }
+};
+
+module.exports = typeaheadController;
--- src/routes/sounds.js
+++ src/routes/sounds.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const getSoundsAPIhandler = require('../handlers/getSoundsAPI');
 const byCategoryHandler = require('../handlers/soundsByCategory');
-
+const typeaheadHandler = require('../handlers/tyeaheadHandler');
 const soundRouter = Router();
 
 // Ruta GET para /sounds
 soundRouter.get('/asmr', getSoundsAPIhandler);
 soundRouter.get('/category/:category', byCategoryHandler);
+soundRouter.get('/typeahead', typeaheadHandler);
 
 module.exports = soundRouter;
